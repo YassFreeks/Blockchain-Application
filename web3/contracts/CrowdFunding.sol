ce2dec56b13f62b400a0e47e8803943c917ce098
YassFreeks <y.moutaouakil@hotmail.com>
2023-03-15 11:49:39 +0100

    Still Building

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -18,11 +18,10 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(adress _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.daedline < block.timestamp, "The deadline should be a date in the future.");
-
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
@@ -30,15 +29,58 @@ contract CrowdFunding {
         campaign.deadline = _deadline;
         campaign.amountCollected = 0;
         campaign.image = _image;
-        
 
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+        
     }
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
-    function getDonators() {}
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory)  {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
     
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 
 
 }
+
+
+// This is a smart contract written in Solidity programming language for a crowdfunding platform. The contract allows users to create campaigns, donate to campaigns, and view campaign information.
+
+// The Campaign struct defines the properties of a campaign, including the owner's address, title, description, target amount, deadline, amount collected, image, donators array, and donations array.
+
+// The mapping variable campaigns is used to store all the campaigns created by users.
+
+// The createCampaign function allows users to create a new campaign by passing in the required parameters. The function first checks if the deadline is in the future and then creates a new Campaign struct with the provided details and stores it in the campaigns mapping. It returns the ID of the newly created campaign.
+
+// The donateToCampaign function allows users to donate to a specific campaign by passing in the campaign ID and sending Ether with the transaction. The function first retrieves the campaign using the ID and then adds the donor's address and donation amount to the campaign's donators and donations arrays. It then sends the donation amount to the campaign owner's address and updates the amountCollected property of the campaign.
+
+// The getDonators function allows users to view the list of donors and their donations for a specific campaign by passing in the campaign ID. It returns two arrays, one containing the donor addresses and the other containing their respective donations.
+
+// The getCampaigns function allows users to view all the campaigns created on the platform. It retrieves all the campaigns from the campaigns mapping and returns an array of Campaign structs.
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
@@ -10,5 +18,16 @@ module.exports = {
     },
   },
 };
+
+
+// This is a configuration file for Hardhat, a popular development environment for Ethereum smart contracts.
+
+// The solidity object is used to configure the Solidity compiler settings. Here, it is set to version 0.8.9.
+
+// The defaultNetwork property specifies which network should be used by default. In this case, it is set to the Goerli test network.
+
+// The networks property is an object that defines the different networks that can be used to deploy and test the smart contracts. In this case, it defines two networks: hardhat and goerli. hardhat is the default network provided by Hardhat for local development and testing. goerli is a public test network that is provided by Ankr. The url property specifies the RPC endpoint of the network, and the accounts property is an array of Ethereum addresses that can be used for deploying and interacting with contracts on the network.
+
+// The settings object is used to configure various settings for the Solidity compiler. Here, the optimizer property is used to enable the optimizer, which is a feature of the Solidity compiler that can improve the gas efficiency of the compiled contracts. The runs property specifies how many times the optimizer should run during compilation. In this case, it is set to 200.
 
 
